a409e2e6b4e67b77bb1746707e616de4d4db20e4
Anderson <84068667+andersonzw@users.noreply.github.com>
2024-05-27 17:15:36 +1000

    bug fixes

--- src/component/ArrowUp.tsx
+++ src/component/ArrowUp.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { IoIosArrowDropup } from "react-icons/io";
 
 const ArrowUp = () => {
--- src/component/BookmarkPopup.tsx
+++ src/component/BookmarkPopup.tsx
@@ -1,34 +1,34 @@
-import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
+// import React from "react";
+// import { motion, AnimatePresence } from "framer-motion";
 
-const BookmarkPopup = ({ isVisible }) => {
-  return (
-    <AnimatePresence>
-      {isVisible && (
-        <motion.div
-          initial={{ x: "100vw" }} // start
-          animate={{ x: 0 }} // end
-          exit={{ opacity: 0 }} // exit
-          transition={{ type: "spring", stiffness: 120, damping: 17 }}
-          style={{
-            padding: "0.8rem 1rem",
-            backgroundColor: "rgb(215 236 255)",
-            position: "fixed",
-            bottom: "5px",
-            right: "10px",
-            zIndex: "120",
-            verticalAlign:"middle",
-            textAlign:"center",
-            borderRadius:"10px",
-            fontSize: "0.8rem",
-            boxShadow:"0px 4px 4px 1px #a4a5be"
-          }}
-        >
-          Added To Bookmarks!
-        </motion.div>
-      )}
-    </AnimatePresence>
-  );
-};
+// const BookmarkPopup = ({ isVisible }) => {
+//   return (
+//     <AnimatePresence>
+//       {isVisible && (
+//         <motion.div
+//           initial={{ x: "100vw" }} // start
+//           animate={{ x: 0 }} // end
+//           exit={{ opacity: 0 }} // exit
+//           transition={{ type: "spring", stiffness: 120, damping: 17 }}
+//           style={{
+//             padding: "0.8rem 1rem",
+//             backgroundColor: "rgb(215 236 255)",
+//             position: "fixed",
+//             bottom: "5px",
+//             right: "10px",
+//             zIndex: "120",
+//             verticalAlign:"middle",
+//             textAlign:"center",
+//             borderRadius:"10px",
+//             fontSize: "0.8rem",
+//             boxShadow:"0px 4px 4px 1px #a4a5be"
+//           }}
+//         >
+//           Added To Bookmarks!
+//         </motion.div>
+//       )}
+//     </AnimatePresence>
+//   );
+// };
 
-export default BookmarkPopup;
+// export default BookmarkPopup;
